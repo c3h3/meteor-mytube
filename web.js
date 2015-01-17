90ee04a263087d506e0e7c8e5de2c12c022a002a
c3h3 <c3h3.tw@gmail.com>
2015-01-17 12:00:29 +0800

    intorduction to each js types in helpers

--- web.js
+++ web.js
@@ -1,8 +1,21 @@
 if (Meteor.isClient){
-  Template.main.helpers({
-    helloString: "Hello MyTube!!"
-  });
   
+  Template.main.var123 = "1234"
+
+  Template.main.helpers({
+    helloString: "1234helloString",
+    helloDate: new Date,
+    helloNumeric: 123456.123,
+    helloArray: ["a","b","c", {a:1234}],
+    helloObject: {a:123,b:234},
+    
+    helloFn: function(xx){
+      return xx+12345
+    }x
+
+  });
+
   console.log("Hello!");
-}
+
+};
 
